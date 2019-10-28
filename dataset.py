c4fce5d4dbfa1f040d59f46b4c9c67f0ee020414
Jinquan Zhang <zhangjq130018@gmail.com>
2019-10-27 22:09:26 -0400

    .

--- dataset.py
+++ dataset.py
@@ -32,36 +32,31 @@ class DataSet():
                 data.append(line.strip())
         return data
 
-    def transfer2Vec(self, words, f):
+    def transfer2Vec(self, words):
         """
         Transfer the words array to vectors array.
-        Parameter f refers to frequency which is used to lower the feature space.
-        Specifically, we will ignore the word whose freqency is lower than f.
         """
-        terms = {}
+        terms = set()
         counts = []
         for word in words:
             terms_in_word = {}
             pword = re.sub(r'[\[\],\"\(\)\.\-:]',' ',word)
             for c in pword.split():
                 if len(c) > 0: 
-                    if terms.get(c,None)==None: terms[c] = 0
-                    terms[c] += 1
-                    if terms_in_word.get(c,None)==None: terms_in_word[c] = 0
-                    terms_in_word[c] += 1
+                    terms.add(c)
+                    terms_in_word[c] = '1'
             counts.append(terms_in_word)
-
-        terms = [t for t in terms.keys() if terms[t] > f]
+        terms = list(terms)
         n = len(terms)
         vectors = []
         for count in counts:
-            vector = [0.0] * n
+            ir = ['0'] * n
             for word in count.keys():
-                if word in terms:
-                    vector[terms.index(word)] = float(count[word])
+                ir[terms.index(word)] = count[word]
+            vector = []
+            for i in range((n // 128) + 1):
+                vector.append(float(int(''.join(ir[i*128:(i+1)*128]),2)))
             vectors.append(vector)
-        # print(n)
-        # print(len([v for v in vectors if (all(np.array(v) <= 0.0)==True)]))
         return vectors
 
 
@@ -71,17 +66,17 @@ class DataSet():
         """
         self.shops = self.loadData('data/shops.csv')
         self.shops = [s.rsplit(',',1)[0][1:-1].replace('\"\"','\"') for s in self.shops[1:]]
-        vectors = self.transfer2Vec(self.shops, 1)
+        vectors = self.transfer2Vec(self.shops)
         for i in range(len(self.shops)): self.shops[i] = [self.shops[i], vectors[i]]
             
         self.item_categories = self.loadData('data/item_categories.csv')
         self.item_categories = [s.rsplit(',',1)[0] for s in self.item_categories[1:]]
-        vectors = self.transfer2Vec(self.item_categories, 1)
+        vectors = self.transfer2Vec(self.item_categories)
         for i in range(len(self.item_categories)): self.item_categories[i] = [self.item_categories[i], vectors[i]]
 
         self.items = self.loadData('data/items.csv')
         self.items = [[s.rsplit(',',2)[0].replace('\"',''), int(s.rsplit(',',2)[2])] for s in self.items[1:]]
-        vectors = self.transfer2Vec([i[0] for i in self.items], 79)
+        vectors = self.transfer2Vec([i[0] for i in self.items])
         for i in range(len(self.items)): self.items[i] += [vectors[i]]
 
 
@@ -96,6 +91,13 @@ class DataSet():
             trainY = np.load('trainDataLabel.npy')
         else:
             rawData = self.loadData('data/sales_train.csv.gz')
+            testData = self.loadData('data/test.csv.gz')
+            shopsInTest = set()
+            for data in testData[1:]:
+                shop_id = int(data.split(',')[1])
+                item_id = int(data.split(',')[2])
+                key = str(shop_id) + ',' + str(item_id)
+                shopsInTest.add(key)
 
             # sum up item_cnt_day to item_month_day and extract price information
             date_blocks = []
@@ -106,11 +108,13 @@ class DataSet():
                 dt = datetime.datetime(date[2],date[1],date[0]).year
                 block_num = int(units[1])
                 shop_id = int(units[2])
+                # if shop_id not in shopsInTest: continue
                 item_id = int(units[3])
                 item_price = float(units[4])
                 item_cnt_day = float(units[5])
-                if item_cnt_day < 0.0: continue
+                # if item_cnt_day < 0.0: continue
                 key = str(shop_id) + ',' + str(item_id)
+                if key not in shopsInTest: continue
                 if self.prices.get(key, None)==None or self.prices[key][1] < dt:
                     self.prices[key] = [item_price, dt]
                 if date_blocks[block_num].get(key, None)==None:
@@ -130,7 +134,9 @@ class DataSet():
                     category_id = self.items[item_id][1]
                     item_price = statistics.mean(date_blocks[i][key][0])
                     item_cnt_month = date_blocks[i][key][1]
-                    features += [float(shop_id), float(item_id), float(category_id), float(year), float(month), item_price]
+                    # features += [float(shop_id), float(item_id), float(category_id)]
+                    features += [float(year), float(month)]
+                    # features.append(item_price)
                     features += self.shops[shop_id][1]
                     features += self.items[item_id][2]
                     features += self.item_categories[category_id][1]
@@ -140,13 +146,13 @@ class DataSet():
             trainY = np.array(trainY)
 
             # Train the model for the price
-            self.priceX = np.delete(trainX, 5, 1)
-            self.priceY = trainX[:,5]
-            model = xgb.XGBRegressor(max_depth = 10, min_child_weight=0.5, \
-                subsample = 1, eta = 0.2, num_round = 1000, seed = 1)
-            print("Train Price Model ...")
-            self.price_model = model.fit(self.priceX, self.priceY)
-            print("Done.")
+            # self.priceX = np.delete(trainX, 5, 1)
+            # self.priceY = trainX[:,5]
+            # model = xgb.XGBRegressor(max_depth = 10, min_child_weight=0.5, \
+            #     subsample = 1, eta = 0.2, num_round = 1000, seed = 1)
+            # print("Train Price Model ...")
+            # self.price_model = model.fit(self.priceX, self.priceY)
+            # print("Done.")
 
             # Save the data for the future convenience
             np.save('trainDataFeatures.npy', trainX)
@@ -175,19 +181,21 @@ class DataSet():
                 shop_id = int(units[1])
                 item_id = int(units[2])
                 category_id = self.items[item_id][1]
-                features += [float(shop_id), float(item_id), float(category_id), float(year), float(month), 0.0]
+                # features += [float(shop_id), float(item_id), float(category_id)]
+                features += [float(year), float(month)]
+                # features.append(0.0)
                 features += self.shops[shop_id][1]
                 features += self.items[item_id][2]
                 features += self.item_categories[category_id][1]
-                key = str(shop_id) + ',' + str(item_id)
-                if self.prices.get(key,None) == None:
-                    # Get the price from model if there is no such a (shop_id, item_id) pair 
-                    # in training data 
-                    testP = np.array([np.delete(features, 5)])
-                    self.prices[key] = [self.price_model.predict(testP)[0],None]
-                    # if ID < 100: print(key + ',' + str(self.prices[key][0]))
-                item_price = self.prices[key][0]
-                features[5] = item_price
+                # key = str(shop_id) + ',' + str(item_id)
+                # if self.prices.get(key,None) == None:
+                #     # Get the price from model if there is no such a (shop_id, item_id) pair 
+                #     # in training data 
+                #     testP = np.array([np.delete(features, 5)])
+                #     self.prices[key] = [self.price_model.predict(testP)[0],None]
+                #     # if ID < 100: print(key + ',' + str(self.prices[key][0]))
+                # item_price = self.prices[key][0]
+                # features[5] = item_price
                 testX.append(np.array(features))
             testX = np.array(testX)
 

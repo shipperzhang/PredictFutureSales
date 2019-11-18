38d16ebffa2a56d94173363c7fa71ac14f3e980e
ShangAtHome <zus223@psu.edu>
2019-11-17 22:05:34 -0500

    mean encoding added

--- model.py
+++ model.py
@@ -7,12 +7,13 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
 import numpy as np
 import logging
+import meanencoding
 
 def XGBRegressor_model(X,Y):
 	# model
 	print('Setting Hyper Parameter ...')
-	model = xgb.XGBRegressor(max_depth = 10, min_child_weight=0.5, \
-		subsample = 1, eta = 0.2, num_round = 1000, seed = 1)
+	model = xgb.XGBRegressor(max_depth = 11, min_child_weight=0.5, \
+		subsample = 1, eta = 0.3, num_round = 1000, seed = 1)
 	print('Train the model ...')
 	model = model.fit(X,Y)
 	# cross validation using mse
@@ -39,14 +40,15 @@ def predict_and_generate_submission_file(model, X):
 	os.system('gzip submission.csv')
 
 if __name__ == "__main__":
-	print("[%s] Initialize the dataset." % logging.time.ctime())
-	dataset = DataSet()
-	print("[%s] Loading Training Data ..." % logging.time.ctime())
-	training_X, training_Y = dataset.loadTrainData(True)
-	print("[%s] Loading Testing Data ..." % logging.time.ctime())
-	testing_X = dataset.loadTestData(True)
+	#print("[%s] Initialize the dataset." % logging.time.ctime())
+	#dataset = DataSet()
+	#print("[%s] Loading Training Data ..." % logging.time.ctime())
+	#training_X, training_Y = dataset.loadTrainData(True)
+	#print("[%s] Loading Testing Data ..." % logging.time.ctime())
+	#testing_X = dataset.loadTestData(True)
+	mean_encoded_trainX, mean_encoded_testX = meanencoding()
 	print("[%s] Train Model ..." % logging.time.ctime())
-	model = XGBRegressor_model(training_X, training_Y)
+	model = XGBRegressor_model(mean_encoded_trainX, training_Y)
 	print("[%s] Predicting ..." % logging.time.ctime())
-	predict_and_generate_submission_file(model, testing_X)
+	predict_and_generate_submission_file(model, mean_encoded_testX)
 	print("[%s] Done." % logging.time.ctime())
